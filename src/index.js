b9a41fd417d13b289ae01f0d364a53f1301244dd
SolloVey <chelnokow@gmail.com>
2023-03-06 06:46:06 +0300

    Add banner with slider and card in menu

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 import './styles/reset.css';
+// core version + navigation, pagination modules:
+import Swiper, { Autoplay, Pagination } from 'swiper';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/autoplay';
+import 'swiper/css/pagination';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+	// slidesPerView: 1,
+	// spaceBetween: 30,
+	// slidesPerGroup: 1,
+	loop: true,
+	autoplay: {
+		delay: 5000,
+		stopOnLastSlide: false,
+		disableOnInteraction: false,
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	// configure Swiper to use modules
+	modules: [Pagination, Autoplay],
+});
 import './styles/fonts.css';
 import './styles/main.scss';
-
-console.log('APP started!!!');
